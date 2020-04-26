1b32a5059b0bbf25ab66d741f10de6d7a66cc03a
Paulo Zacarias <paulo.zacarias@outlook.pt>
2020-04-26 20:37:25 +0300

    Corrected sprint validation method. Added functionality to add user who creates task as assigned person. Added query set option to retrieve list of sprints by project_id.

--- api/projects/serializers.py
+++ api/projects/serializers.py
@@ -2,26 +2,38 @@ from .models import Project, Sprint, Task
 from rest_framework import serializers
 
 
-class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+class ProjectSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Project
-        fields = ['url', 'name', 'description', 'admin', 'participants']
+        fields = ['id', 'name', 'description', 'admin', 'participants']
 
         # The admin will be automatically assigned to the currently logged in user,
         # this field will be added through the Project's view (create method) and is not required in the serializer
         extra_kwargs = {'admin': {'required': False}}
 
 
-class SprintSerializer(serializers.HyperlinkedModelSerializer):
+class SprintSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Sprint
-        fields = ['url', 'start_date', 'end_date', 'planned_story_points', 'project']
+        fields = ['id', 'start_date', 'end_date', 'planned_story_points', 'project']
+
+    def validate(self, data):
+        """
+        Check that the start is before the stop.
+        """
+        if data['start_date'] > data['end_date']:
+            raise serializers.ValidationError("Start date must be earlier than end date.")
+        return data
 
 
-class TaskSerializer(serializers.HyperlinkedModelSerializer):
+class TaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Task
-        fields = ['url', 'title', 'description', 'weight', 'story_points', 'status', 'sprint', 'assigned_person']
+        fields = ['id', 'title', 'description', 'weight', 'story_points', 'status', 'sprint', 'assigned_person']
+
+        # The assigned person will be automatically assigned to the currently logged in user,
+        # this field will be added through the Project's view (create method) and is not required in the serializer
+        extra_kwargs = {'assigned_person': {'required': False}}
--- api/projects/urls.py
+++ api/projects/urls.py
@@ -4,10 +4,10 @@ from .views import ProjectViewSet, SprintViewSet, TaskViewSet
 
 router = routers.DefaultRouter()
 router.register(r'projects', ProjectViewSet)
-router.register(r'sprints', SprintViewSet)
+# router.register(r'sprints', SprintViewSet)  # 'basename' allows the queryset to be override in view
+router.register(r'sprints', SprintViewSet, basename='sprints')  # 'basename' allows the queryset to be override in view
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
