8e71a86eb55a1533d459d1de7bccec36a4654155
Paulo Zacarias <paulo.zacarias@ericsson.com>
2020-04-27 23:46:41 +0300

    Implemented user's password change endpoint. Fixed mistake on UserSerializer. Code clean up.

--- api/users/views.py
+++ api/users/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from .serializers import UserSerializer, UserSerializerUpdate, ProfileSerializer
+from rest_framework.views import APIView
+
+from .serializers import UserSerializer, UserSerializerUpdate, ProfileSerializer, UpdatePasswordSerializer
 from .permissions import IsAdminOrIsSelf
 from .models import Profile
 
@@ -52,6 +54,33 @@ class ProfileImageUpdate(generics.UpdateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated, IsAdminOrIsSelf]
+
+
+class UpdatePassword(APIView):
+    """
+    An endpoint for changing password.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def put(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = UpdatePasswordSerializer(data=request.data)
+
+        if serializer.is_valid():
+            # Check old password
+            old_password = serializer.data.get("old_password")
+            if not self.object.check_password(old_password):
+                return Response({"old_password": ["Wrong password."]},
+                                status=status.HTTP_400_BAD_REQUEST)
+            # set_password also hashes the password that the user will get
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CustomAuthToken(ObtainAuthToken):
